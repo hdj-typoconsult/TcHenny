c60f62ee8973fd322ab2f8a27fa6c1a855ce7a53
hdj-typoconsult <hdj@typoconsult.dk>
2021-11-05 13:35:44 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,13 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TC Henny',
+    'description' => 'My very first composer installed local extension',
+    'category' => 'misc',
+    'author' => 'Henny Jensen',
+    'author_email' => 'hdj@typoconsult.dk',
+    'author_company' => 'TypoConsult A/S',
+    'state' => 'excludeFromUpdates',
+    'clearCacheOnLoad' => true,
+    'version' => '11.0.0',
+];
